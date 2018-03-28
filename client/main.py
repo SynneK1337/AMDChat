f61a1b81583d44819fbe630660d8b5d67f7054a3
SynneK1337 <276786@pw.edu.pl>
2018-03-29 00:03:36 +0200

    update client, problem with receiving

--- client/main.py
+++ client/main.py
@@ -9,6 +9,7 @@ class Client(asyncore.dispatcher):
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
+        self.socket.setblocking(True)
         try:
             self.connect((hostname, 1337))
             self.send(nickname.encode('utf-8'))
@@ -17,5 +18,13 @@ class Client(asyncore.dispatcher):
         else:
             print("Connected successful")
 
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            print(data.decode('utf-8'))
+    
+    def handle_write(self):
+        msg = input("%s@%s$ " % (nickname, hostname))
+        self.send(msg.encode('utf-8'))
 c = Client(hostname, nickname)
 asyncore.loop()
