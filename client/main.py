c0c1d99536f04806e09f094766a919cd8213ef94
SynneK1337 <276786@pw.edu.pl>
2018-04-24 21:17:06 +0200

    NICKNAMES ARE WORKING FINALLY JP2GMD

--- client/main.py
+++ client/main.py
@@ -5,7 +5,6 @@ import threading
 hostname = input("Server: ")
 nickname = input("Nickname: ")
 
-
 class Client(asyncore.dispatcher):
     def __init__(self):
         asyncore.dispatcher.__init__(self)
@@ -19,18 +18,18 @@ class Client(asyncore.dispatcher):
             print("Connection failed " + "Error: " + str(e))
         else:
             print("Connected successful")
+        self.server_name = self.recv(64).decode('utf-8')
 
     def receiving(self):
         while 1:
             data = self.recv(1024)
             if data:
-                print(data.decode('utf-8'))
+                print("\n" + data.decode('utf-8'))
 
     def sending(self):
         while 1:
-            msg = input("%s@%s$ " % (nickname, hostname))
-            self.send(msg.encode('utf-8'))
-
+            msg = input("{}@{}$ ".format(nickname, self.server_name))
+            self.send(("{}@{}$ ".format(nickname, self.server_name) + msg).encode('utf-8'))
 
 c = Client()
 t1 = threading.Thread(target=c.receiving)
--- server/main.py
+++ server/main.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 import asyncore
 import socket
-from time import sleep
-host = ''
+host = 'localhost'
 port = 1337
-
+server_name = "AMDChat"
 
 class Server(asyncore.dispatcher):
     clients = []
+    nicknames = []
 
     def __init__(self):
         asyncore.dispatcher.__init__(self)
@@ -20,35 +20,30 @@ class Server(asyncore.dispatcher):
     def handle_accept(self):
         sock, addr = self.accept()
         sock.setblocking(True)
-        sock.send("Welcome!".encode('utf-8'))
+        sock.send(server_name.encode('utf-8'))
         nickname = sock.recv(64)
         while not nickname:
             nickname = sock.recv(64)
-        if len(self.clients) > 0:
-            for x in self.clients:
-                if nickname in x:
-                    print("%s nickname spoofing detected from %s" %
-                          (nickname.decode('utf-8'), addr[0]))
-                    sock.close()
-                else:
-                    self.clients.append((EchoHandler(sock), nickname))
-                    print("[+] %s connected from %s" %
-                          (nickname.decode('utf-8'), addr[0]))
-        else:
-            self.clients.append((EchoHandler(sock), nickname))
-            print("[+] %s connected from %s" %
+        if nickname in self.nicknames:
+            print("[!] %s nickname spoofing detected from %s" %
                   (nickname.decode('utf-8'), addr[0]))
+            sock.send("Nickname is used already".encode('utf-8'))
+
+        else:
+            self.nicknames.append(nickname)
+            self.clients.append(EchoHandler(sock))
+            print("[i] %s connected from %s" % (nickname.decode('utf-8'), addr[0]))
 
 
 class EchoHandler(asyncore.dispatcher_with_send):
     def handle_read(self):
         data = self.recv(1024)
         if data:
-            print(data)
+            print(data.decode('utf-8'))
             for x in Server.clients:
                 try:
-                    if x[0] != self:
-                        x[0].send(data)
+                    if x != self:
+                        x.send(data)
                 except Exception as e:
                     print("Server Closing. " + "Error: " + str(e))
                     Server.clients.remove(x)
